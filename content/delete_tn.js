a91e23a5ad0c2e5da08a96ddf1aae5dfd6e26a0f
hallo-py <13711216166@163.com>
2026-04-04 14:29:59 +0800

    解决百度网址参数过长的问题

--- content/delete_tn.js
+++ content/delete_tn.js
@@ -1,6 +1,21 @@
 const params = new URLSearchParams(window.location.search);
 
-if (params.has('tn')) {
-    params.delete('tn');
-    window.location.href = `${window.location.origin}${window.location.pathname}?${params.toString()}#`
+// https://www.baidu.com/s?wd=python&pn=10&ie=utf-8
+// 仅保留 wd , pn , ie  参数
+
+let newParams = new URLSearchParams();
+
+if (params.has('wd')) {
+    newParams.set('wd', params.get('wd'));
+}
+if (params.has('ie')) {
+    newParams.set('ie', params.get('ie'));
+}
+if (params.has('pn') && params.get('pn') !== '0') {
+    newParams.set('pn', params.get('pn'));
+}
+
+let newURL = `https://www.baidu.com/s?${newParams.toString()}#`;
+if (window.location.href !== newURL) {
+    window.location.href = newURL;
 }
